317b6e9880a15e0c1f3c427b4bea44e98a7fd884
EDGExD <76109839+EDGExD@users.noreply.github.com>
2021-05-14 17:38:09 +0200

    Dodanie testu.

--- testsReactor/src/test/java/edu/iis/mto/oven/OvenTest.java
+++ testsReactor/src/test/java/edu/iis/mto/oven/OvenTest.java
@@ -7,7 +7,11 @@ import static org.hamcrest.Matchers.equalTo;
 import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import static org.mockito.ArgumentMatchers.any;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
@@ -25,13 +29,22 @@ class OvenTest {
     }
     
     @Test
-    void test_1() {
+    void test_1_Oven_Exception() throws HeatingException {
+        //HeatingSettings heatingSettings = HeatingSettings.builder().withTargetTemp(200).withTimeInMinutes(20).build();
+        Mockito.doThrow(new HeatingException()).when(heatingModule).termalCircuit(any());
+        
         List<ProgramStage> stagesList = new ArrayList<ProgramStage>();
         stagesList.add(ProgramStage.builder().withHeat(HeatType.THERMO_CIRCULATION).withTargetTemp(200).withStageTime(20).build());
         BakingProgram program = BakingProgram.builder().withInitialTemp(180).withStages(stagesList).build();
+        
         Oven piekarnik = new Oven(heatingModule, fan);
-        piekarnik.start(program);
-        MatcherAssert.assertThat(true, equalTo(true));
+        try{
+            piekarnik.start(program);
+        }
+        catch(OvenException e){
+            MatcherAssert.assertThat(true, equalTo(true));
+        }
+        MatcherAssert.assertThat(false, equalTo(true));
     }
 
 }
