886e4cfda4bdb5cd1e61a391bbe4e8cfc5feee92
EDGExD <76109839+EDGExD@users.noreply.github.com>
2021-05-14 16:31:43 +0200

    Zapoznanie się z kodem i ogólne sprawdzenie działania.

--- testsReactor/src/test/java/edu/iis/mto/oven/OvenTest.java
+++ testsReactor/src/test/java/edu/iis/mto/oven/OvenTest.java
@@ -1,17 +1,36 @@
 package edu.iis.mto.oven;
 
+import java.util.ArrayList;
+import java.util.List;
 import static org.hamcrest.Matchers.equalTo;
 
 import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class OvenTest {
 
+    @Mock
+    HeatingModule heatingModule;
+    
+    @Mock
+    Fan fan;
+    
     @Test
     void itCompiles() {
+        MatcherAssert.assertThat(true, equalTo(true));
+    }
+    
+    @Test
+    void test_1() {
+        List<ProgramStage> stagesList = new ArrayList<ProgramStage>();
+        stagesList.add(ProgramStage.builder().withHeat(HeatType.THERMO_CIRCULATION).withTargetTemp(200).withStageTime(20).build());
+        BakingProgram program = BakingProgram.builder().withInitialTemp(180).withStages(stagesList).build();
+        Oven piekarnik = new Oven(heatingModule, fan);
+        piekarnik.start(program);
         MatcherAssert.assertThat(true, equalTo(true));
     }
 
